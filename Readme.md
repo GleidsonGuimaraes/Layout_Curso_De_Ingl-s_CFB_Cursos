56bf28aae476d88d35bf675769170740c6f31ca3
Gleidson Guimarães <gleidson.g.f@hotmail.com>
2021-12-22 20:26:48 -0300

    Primeiro commit

---
+++ Readme.md
@@ -0,0 +1,5 @@
+# Layout Escola de Inglês
+
+## Links úteis
+
+* https://www.youtube.com/watch?v=8dPpZsC6Vg8&list=PLx4x_zx8csUgS5ioRmQVBeGHmKBQEyNXQ
---
+++ js/script.js
@@ -0,0 +1,13 @@
+let cont = 1;
+
+function autoTrocar() {
+    let autoT = setInterval(trocar, 3000);
+}
+
+function trocar() {
+    document.getElementById("banner").style.backgroundImage = "url(/img/banner" + cont + ".jpg";
+    cont++;
+    if(cont > 3){
+        cont = 1;
+    }
+}
